ec6a7d85e9171b6f7f258bc7a477ff89e134ce95
Wide97 <mwidesott@gmail.com>
2024-10-21 14:22:07 +0200

    inizialize

---
+++ src/main/java/marcowidesott/backm2w1d1/Backm2w1d1Application.java
@@ -0,0 +1,13 @@
+package marcowidesott.backm2w1d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backm2w1d1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backm2w1d1Application.class, args);
+	}
+
+}
